a012f2f941ee41c7bfb6f85add208d2183f62f50
Putnam1 <mat.rebraton@gmail.com>
2021-12-01 10:46:13 +0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=8970bfa960387ab8ccd8&state=88e11a1c-1487-46a6-baa5-f02f27e349ef

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-setInterval(setClock, 1000)
-
-const hourHand = document.querySelector('[data-hour-hand]')
-const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
-
 function setClock() {
+  const hourHand = document.querySelector('[data-hour-hand]')
+  const minuteHand = document.querySelector('[data-minute-hand]')
+  const secondHand = document.querySelector('[data-second-hand]')
   const currentDate = new Date()
   const secondsRatio = currentDate.getSeconds() / 60
   const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
@@ -19,3 +16,34 @@ function setRotation(element, rotationRatio) {
 }
 
 setClock()
+
+function setTime(){
+  const time = document.querySelector('#time');
+
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let dayName = date.getDay();
+  let dayNum = date.getDate();
+  let mounth = date.getMonth();
+  
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  days = ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'];
+  mounths = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+
+  time.textContent = hours + ":" + minutes + ":" + seconds + " " + days[dayName] + ", " + dayNum + " " + mounths[mounth];
+}
+setInterval(()=>{
+  setTime()
+  setClock()
+});
